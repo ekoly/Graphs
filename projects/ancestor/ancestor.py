e0ab6fd8741aea110b8bb0e5f5b6d0c848f06c92
Erik <ecowley@protonmail.com>
2020-07-14 18:45:50 -0400

    assignment 2

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,20 @@
+from collections import defaultdict
 
 def earliest_ancestor(ancestors, starting_node):
-    pass
+
+    child_to_parents = defaultdict(lambda: set())
+
+    for parent, child in ancestors:
+        child_to_parents[child].add(parent)
+
+    if starting_node not in child_to_parents:
+        return -1
+
+    frontier = list(child_to_parents[starting_node])
+    while True:
+        new_frontier = []
+        for node in frontier:
+            new_frontier.extend(list(child_to_parents[node]))
+        if not new_frontier:
+            return frontier[-1]
+        frontier = new_frontier
